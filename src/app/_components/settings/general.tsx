23e6242b8b0b3e4eacbfe1f89c700358fde5f04e
Chaoscaot <chaos@chaoscaot.de>
2025-07-20 17:26:27 +0200

    fix: remove unused import of Session type in general settings component

--- src/app/_components/settings/general.tsx
+++ src/app/_components/settings/general.tsx
@@ -12,7 +12,6 @@ import {
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import type { Session } from "next-auth";
 import { Input } from "~/components/ui/input";
 import React from "react";
 import { Button } from "~/components/ui/button";
