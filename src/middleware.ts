0a659278d86c59860e002730a2e4c0acb27da927
Chaoscaot <max@maxsp.de>
2025-12-07 00:04:10 +0100

    Fix React

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,19 +13,14 @@ const isProtectedRoute = createRouteMatcher([
   "/settings",
 ]);
 
-export default convexAuthNextjsMiddleware(
-  async (request, { convexAuth }) => {
-    if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
-      return nextjsMiddlewareRedirect(request, "/dashboard");
-    }
-    if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-      return nextjsMiddlewareRedirect(request, "/");
-    }
-  },
-  {
-    verbose: true,
-  },
-);
+export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+  if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
+    return nextjsMiddlewareRedirect(request, "/dashboard");
+  }
+  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+});
 
 export const config = {
   // The following matcher runs middleware on all routes
